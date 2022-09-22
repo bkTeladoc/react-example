ddcc1d387bff7ee5de37687782e60f2aa1fc4805
Youtapl <Youta_pl@live.com>
2022-09-22 15:50:22 +0200

    First Version

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+const Header = () => {
+    
+    return(
+        <Jumbotron>
+            <h1>Bogdan Klekot</h1>
+            <p>Example Rect application.</p>
+        </Jumbotron>
+    )
+}
+
+export default Header
